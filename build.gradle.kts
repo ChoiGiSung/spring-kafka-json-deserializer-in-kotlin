8ead4cd165bae7cb0ac91880dea7dd06564c02b2
DESKTOP-MJ25L1Q\chlrl <gisungcu@gmail.com>
2023-09-29 22:41:03 +0900

    multi module로 구성
    - 각 케이스별로 모듈 만들기
    - 브랜치로 나눠서 하는 거 보다 한번에 모든 케이스를 테스트할 수 있다는 장점

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,39 +1,45 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.1.4"
-	id("io.spring.dependency-management") version "1.1.3"
+	id("org.springframework.boot") version "2.7.16" apply false
+	id("io.spring.dependency-management") version "1.1.3" apply false
 	kotlin("jvm") version "1.8.22"
-	kotlin("plugin.spring") version "1.8.22"
+	kotlin("plugin.spring") version "1.8.22" apply false
 }
 
-group = "com.example"
-version = "0.0.1-SNAPSHOT"
+allprojects {
+	group = "com.example"
+	version = "0.0.1-SNAPSHOT"
 
-java {
-	sourceCompatibility = JavaVersion.VERSION_17
-}
-
-repositories {
-	mavenCentral()
-}
-
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.springframework.kafka:spring-kafka")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.kafka:spring-kafka-test")
-}
-
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
+	repositories {
+		mavenCentral()
 	}
+
+	tasks.withType<KotlinCompile> {
+		kotlinOptions {
+			freeCompilerArgs += "-Xjsr305=strict"
+			jvmTarget = "11"
+		}
+	}
+
+	tasks.withType<Test> {
+		useJUnitPlatform()
+	}
+
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+subprojects {
+	apply(plugin = "org.springframework.boot")
+	apply(plugin = "io.spring.dependency-management")
+	apply(plugin = "org.jetbrains.kotlin.jvm")
+	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+
+	dependencies {
+		implementation("org.springframework.boot:spring-boot-starter-web")
+		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+		implementation("org.jetbrains.kotlin:kotlin-reflect")
+		implementation("org.springframework.kafka:spring-kafka")
+		testImplementation("org.springframework.boot:spring-boot-starter-test")
+		testImplementation("org.springframework.kafka:spring-kafka-test")
+	}
 }
