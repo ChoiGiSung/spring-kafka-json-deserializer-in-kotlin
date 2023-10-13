0651fdfd5e0b9019d2085f94b033e00c5747534f
DESKTOP-MJ25L1Q\chlrl <gisungcu@gmail.com>
2023-10-13 21:26:05 +0900

    update producer json mapping

--- customProducer/src/main/kotlin/com/example/kafka/KafkaConfig.kt
+++ customProducer/src/main/kotlin/com/example/kafka/KafkaConfig.kt
@@ -11,6 +11,10 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.core.ProducerFactory
+import org.springframework.kafka.support.converter.RecordMessageConverter
+import org.springframework.kafka.support.converter.StringJsonMessageConverter
+import org.springframework.kafka.support.mapping.DefaultJackson2JavaTypeMapper
+import org.springframework.kafka.support.mapping.Jackson2JavaTypeMapper
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer
 import org.springframework.kafka.support.serializer.JsonDeserializer
 import org.springframework.kafka.support.serializer.JsonSerializer
@@ -20,16 +24,48 @@ class KafkaConfig(
     val objectMapper: ObjectMapper
 ) : DefaultKafkaProducerFactoryCustomizer {
 
+    /**
+     * TYPE_MAPPINGS을 커스텀 하는 방법 3가지
+     * idClassMapping을 어떻게 넣어주냐의 차이.
+     */
+
+
+
+    /**
+     * case 1 : producerFactory의 설정 업데이트. 어차피 JsonSerializer가 사용하는 값임.
+     * 지금은 하드 코딩이지만 동적으로 발송될 Class의 path을 읽어서 value를 만들어 줄 수 있도록 개발할 수 있다.
+     */
+//    override fun customize(producerFactory: DefaultKafkaProducerFactory<*, *>?) {
+//        (producerFactory as DefaultKafkaProducerFactory<Any, Any>).valueSerializer = JsonSerializer(objectMapper)
+//        producerFactory.updateConfigs(mapOf(JsonSerializer.TYPE_MAPPINGS to "CREATE:class.~~~,UPDATE:class.~~~"))
+//    }
+
+
+    /**
+     * case 2 : jsonSerializer의 typeMapper를 오버라이딩 한다.
+     */
     override fun customize(producerFactory: DefaultKafkaProducerFactory<*, *>?) {
-        (producerFactory as DefaultKafkaProducerFactory<Any, Any>).valueSerializer = JsonSerializer(objectMapper)
+        val jsonSerializer = JsonSerializer<Any>(objectMapper)
+        jsonSerializer.typeMapper = typeMapper()
+
+        (producerFactory as DefaultKafkaProducerFactory<Any, Any>).valueSerializer = jsonSerializer
     }
 
-    fun producerFactory(): MutableMap<String, Any> {
-        val config = mutableMapOf<String, Any>()
-        config[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = "127.0.0.1:9092"
-        config[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
-        config[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JsonDeserializer::class.java
-        return config
+    private fun typeMapper(): DefaultJackson2JavaTypeMapper {
+        val typeMapper = DefaultJackson2JavaTypeMapper()
+        val mappings: MutableMap<String, Class<*>> = mutableMapOf()
+        mappings["CREATE"] = CustomProducer.UserInfo::class.java
+        mappings["UPDATE"] = CustomProducer.OtherUserInfo::class.java
+        typeMapper.typePrecedence = Jackson2JavaTypeMapper.TypePrecedence.TYPE_ID
+        typeMapper.idClassMapping = mappings
+        return typeMapper
     }
+
+
+
+    /**
+     * case 3 : application.properties에서 주석 부분 작성
+     */
+
 
 }
